14f80793135929499e889192964f6af0c9e6d825
andresrh11 <andresr10020@gmail.com>
2022-12-02 22:56:42 -0500

    save2

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,17 @@
 import BuyButton from "./BuyButton";
-import img from '../utils/sequelize.png'
+import img from "../utils/sequelize.png";
 export default function Card() {
   return (
     <>
       <div className="flex border-red-500">
-        <div><img src={img} alt="img" /></div>
         <div>
-          <div><h1>Titulo</h1></div>
+          <img src={img} alt="img" />
+        </div>
+        <div>
+          <div>
+            <h1>Titulo</h1>
+            <img src="" alt="" />
+          </div>
         </div>
         <BuyButton />
       </div>
